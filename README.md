942fa43f2149106d97a3cebbc0db5c68bdca2b15
Robert Prosenc <976156+rprosenc@users.noreply.github.com>
2021-12-05 12:58:03 +0100

    solved day4/Part II

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 ![Day](https://badgen.net/badge/01/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
-[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%86/yellow)](src/day04)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -90,9 +90,9 @@ Both parts: -
 
 ```
 Day 04
-Time part 1: 9.24ms
-Time part 2: -
-Both parts: 9.24ms
+Time part 1: 27.53ms
+Time part 2: 24.93ms
+Both parts: 52.46ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 1/50
-Total time: 9.24ms
+Total stars: 2/50
+Total time: 52.46ms
 ```
 
 <!--/RESULTS-->
--- src/day04/index.js
+++ src/day04/index.js
@@ -15,8 +15,14 @@ const part1 = (rawInput) => {
 
 const part2 = (rawInput) => {
     const input = parseInput(rawInput);
+    const numbers = parseNumbers(input);
+    let boards = parseBoards(input);
+    boards = solveBoards(numbers, boards);
 
-    return;
+    boards = boards.sort((a,b) => { return a.winOrder < b.winOrder ? -1 : 1 });
+
+    const lastWinner = boards[boards.length-1];
+    return countRemaining(lastWinner) * lastWinner.drawn;
 };
 
 const parseNumbers = (input) => {
@@ -133,7 +139,25 @@ run({
     },
     part2: {
         tests: [
-            // { input: ``, expected: "" },
+            { input: `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+      22 13 17 11  0
+       8  2 23  4 24
+      21  9 14 16  7
+       6 10  3 18  5
+       1 12 20 15 19
+      
+       3 15  0  2 22
+       9 18 13 17  5
+      19  8  7 25 23
+      20 11 10 24  4
+      14 21 16 12  6
+      
+      14 21 17 24  4
+      10 16 15  9 19
+      18  8 23 26 20
+      22 11 13  6  5
+       2  0 12  3  7`, expected: 1924 },
         ],
         solution: part2,
     },
